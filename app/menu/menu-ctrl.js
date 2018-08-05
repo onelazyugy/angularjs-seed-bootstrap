d0756a6228b7446b4918310ad57d8b26bd6971e3
onelazyugy <you@example.com>
2018-08-04 21:38:49 -0400

    added responsive menu

---
+++ app/menu/menu-ctrl.js
@@ -0,0 +1,3 @@
+angular.module("myApp").controller("menuCtrl", ["$scope", "toolService", function($scope, toolService){
+  // $scope.menu = "I am a menu";
+}]);
--- app/rental/controller/rental-ctrl.js
+++ app/rental/controller/rental-ctrl.js
@@ -1,4 +1,4 @@
 angular.module("myApp.rental").controller("rentalCtrl", ["$scope", "toolService", function($scope, toolService){
     $scope.title = "Tool For Rent";
-    $scope.tools = toolService.getTools().length === 0 ? 'No tool available' : toolService.getTools();
+    $scope.tools = toolService.getTools().length === 0 ? [] : toolService.getTools();
 }]);
